fb6be6ca21049bffd328a4dd5d9025244b57cf15
senura adithya <138124767+senu2000@users.noreply.github.com>
2024-05-06 20:22:45 +0530

    change the path

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
                 <Route path={"/login"} element={<Login/>}/>
                 <Route path={"/register"} element={<Register/>}/>
                 <Route path={"/cart"} element={<Cart/>}/>
-                <Route path={"/payment"} element={<Payment/>}/>
+                <Route path={"/paymen"} element={<Payment/>}/>
             </Routes>
         </BrowserRouter>
     </>
