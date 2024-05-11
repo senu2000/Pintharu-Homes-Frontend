66bec41bbe0f040d48b66a3cc43f6a869c34e83e
#Des|-| <137793168+nipunadeshan20@users.noreply.github.com>
2024-05-11 19:58:09 +0530

    home5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,15 @@
 import { Footer } from "flowbite-react";
+import Img2 from "../../public/Images/Logo1.png";
+import React from "react";
+import '../css/Footer.css'
 
 export default function MyFooter() {
     return (
         <Footer container>
             <div className="w-full text-center">
                 <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-                    <Footer.Brand
-                        href="https://flowbite.com"
-                        src="https://flowbite.com/docs/images/logo.svg"
-                        alt="Flowbite Logo"
-                        name="Flowbite"
-                    />
-                    <Footer.LinkGroup>
+                    <img src={Img2} className="editlogo1" alt="Logo" />
+                    <Footer.LinkGroup className="footerlinksedit">
                         <Footer.Link href="#">About</Footer.Link>
                         <Footer.Link href="#">Privacy Policy</Footer.Link>
                         <Footer.Link href="#">Licensing</Footer.Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import "../css/Home.css";
 import MyFooter from "../components/Footer";
 import MyNavBar from "../components/NavBar.jsx";
 import AdsSection from "../components/Carousel.jsx";
+import SearchBar from "../components/SearchBar.jsx";
+import PaintProdcutCard from "../components/PaintProdcutCard.jsx";
+import React from "react";
+import {Button} from "flowbite-react";
+import OutlinedButtons from "../components/MainBtn.jsx";
 
 
 export default function Home() {
@@ -18,7 +23,25 @@ export default function Home() {
                         <AdsSection/>
                     </div>
                 </div>
+                <div className="editsearchbar">
+
+                    <SearchBar/></div>
             </div>
+            <div className="homeproductssection">
+                <p className="homeproducttitle">FEATURED PRODUCTS</p>
+                <p className="homeproductsubtitle">Explore Our Selection of Featured Products</p>
+                <div className="paintproductcards">
+                    <tr>
+                        <td className="paintcard1"><PaintProdcutCard/></td>
+                        <td className="paintcard2"><PaintProdcutCard/></td>
+                        <td className="paintcard3"><PaintProdcutCard/></td>
+                        <td className="paintcard4"><PaintProdcutCard/></td>
+                    </tr>
+                </div>
+                <div className="">
+                <button type="submit" className="exploremorebtn">Explore More</button>
+                </div>
+                </div>
 
             <div><MyFooter/></div>
 
