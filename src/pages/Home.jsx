1a7096655cd28dfac36c8bc723737e7c8b8d4a9f
#Des|-| <137793168+nipunadeshan20@users.noreply.github.com>
2024-05-17 00:07:56 +0530

    home5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import MyNavBar from "../components/NavBar.jsx";
 import AdsSection from "../components/Carousel.jsx";
 import SearchBar from "../components/SearchBar.jsx";
 import PaintProdcutCard from "../components/PaintProdcutCard.jsx";
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import ProjectCountSection from "../components/ProjectCountSection.jsx";
 import {Button} from "flowbite-react";
-import OutlinedButtons from "../components/MainBtn.jsx";
 
 
 export default function Home() {
@@ -30,6 +30,7 @@ export default function Home() {
             <div className="homeproductssection">
                 <p className="homeproducttitle">FEATURED PRODUCTS</p>
                 <p className="homeproductsubtitle">Explore Our Selection of Featured Products</p>
+                {/*<div className="productssectionline"></div>*/}
                 <div className="paintproductcards">
                     <tr>
                         <td className="paintcard1"><PaintProdcutCard/></td>
@@ -38,10 +39,51 @@ export default function Home() {
                         <td className="paintcard4"><PaintProdcutCard/></td>
                     </tr>
                 </div>
-                <div className="">
-                <button type="submit" className="exploremorebtn">Explore More</button>
+                <div className="container">
+                    <button className="noselect animatedbtn1">Show More<span className="animatedbtn"> >>></span>
+                    </button>
                 </div>
+            </div>
+            <div>
+                <div>
+                    <ProjectCountSection/>
                 </div>
+                <div className="projectshomesection">
+                    <div className="projectssectiontop"></div>
+                    <tr>
+                        <td className="td"></td>
+                        <td className="td"></td>
+                        <td className="td">
+                            <div className="projectssection">
+                                <div className="projectsectiontitle1">DISCOVER</div>
+                                <div className="projectsectiontitle2"> Painted Masterpieces</div>
+                                <p className="text-black">Explore our painted wonders in this showcase of vibrant
+                                    transformations.
+                                    Witness how we bring imagination to life with each stroke. Our projects stand
+                                    as testaments to our dedication to excellence and client satisfaction.</p>
+                                <div className="/">
+                                    <button className="noselect animatedbtn2">View Projects<span
+                                        className="animatedbtn"> >>></span></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <div className="quotationhomesection">
+                        <div className="quotationsectiontitle">Calculate Your Paint Project Cost</div>
+                        <div className="quotationgeneratorline"></div>
+                        <div className="quotationsectiontext">Discover precision in painting costs with our Quotation
+                            Generator! Quickly input wall dimensions and receive accurate estimates, empowering
+                            confident project planning. Start now to transform your space with certainty.
+                        </div>
+                        <div className="container2">
+                            <button className="noselect animatedbtn2">Calculate<span className="animatedbtn"> >>></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*<div>hdbbweb</div>*/}
+
 
             <div><MyFooter/></div>
 
